7374f3cc2213628de2bcd2df3cc0c6c9acdd152f
ZachTheis <zach.theis+zacharywstheis@gmail.com>
2020-10-16 04:11:57 -0400

    New methods
    Added and tested numberOf Accesses, busiestHour, busiestTwoHours, and quietestHour, forgetting to commit after each

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -22,6 +22,17 @@ public class LogAnalyzer
         // Create the reader to obtain the data.
         reader = new LogfileReader("demo.log");
     }
+    
+    /**
+     * Overloaded constructor
+     * @param The name of the file.
+     * Satisfies 7.12
+     */
+    public LogAnalyzer(String fileName)
+    {
+        hourCounts = new int[24];
+        reader = new LogfileReader(fileName);
+    }
 
     /**
      * Analyze the hourly access data from the log file.
@@ -54,5 +65,81 @@ public class LogAnalyzer
     public void printData()
     {
         reader.printData();
+    }
+    
+    /**
+     * Return the number of accesses recorded in the log file, once it has
+     * been analyzed.
+     * Satisfies 7.14
+     * @return The total number of recorded accesses.
+     */
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        for(int hour = 0; hour < hourCounts.length; hour++)
+        {
+            //System.out.println(hourCounts[hour]);
+            total += hourCounts[hour];
+        }
+        return total;
+    }
+    
+    /**
+     * Finds and returns the busiest hour.
+     * This satisfies 7.15
+     * @return The busiest hour, based on a 24-hr clock.
+     */
+    public int busiestHour()
+    {
+        int busiest = 0;
+        int firstPlace = hourCounts[0];
+        for(int hour = 1; hour < hourCounts.length; hour++)
+        {
+            if(hourCounts[hour] > firstPlace)
+            {
+                firstPlace = hourCounts[hour];
+                busiest = hour;
+            }
+        }
+        return busiest;
+    }
+    
+    /**
+     * Finds and returns the busiest two-hour period based on a 24-hr clock.
+     * This satisfies 7.18
+     * @return The first hour of the two-hour period.
+     */
+    public int busiestTwoHours()
+    {
+        int busiest = 23;
+        int firstPlace = hourCounts[0] + hourCounts[23];
+        for(int hour = 0; hour < hourCounts.length; hour++)
+        {
+            if(hourCounts[hour] + hourCounts[hour + 1] > firstPlace)
+            {
+                busiest = hour;
+            }
+        }
+        return busiest;
+    }
+    
+    /**
+     * Finds and returns the quietest hour.
+     * This satisfies 7.16.
+     * @return The quiestest hour, based on a 24-hr clock.
+     */
+    public int quietestHour()
+    {
+        int quietest = 0;
+        int lastPlace = hourCounts[0];
+        for(int hour = 1; hour < hourCounts.length; hour++)
+        {
+            if(hourCounts[hour] < lastPlace)
+            {
+                lastPlace = hourCounts[hour];
+                quietest = hour;
+            }
+        }
+        return quietest;
     }
 }
